d01f1678d5c6453a7460bed699d9c242fe4b750b
hjalitto <45777577+hjalitto@users.noreply.github.com>
2020-01-25 19:49:36 -0800

    able to open an smr(x) file (i think)

--- CED/createCEDWrapper.py
+++ CED/createCEDWrapper.py
@@ -6,5 +6,16 @@ import sys
 import struct
 print(os.getcwd())
 # lib = WinDLL("C:\\Henry\\PythonProjects\\CED\\CEDS64ML\\x64\\ceds64int.dll")
-LibraryLoader(WinDLL).LoadLibrary("C:\\Henry\\PythonProjects\\CED\\CEDS64ML\\x86\\ceds64int.dll")
-# lib = WinDLL("C:\\CEDMATLAB\\CEDS64ML\\x64\\ceds64int.dll")
+ced_lib = LibraryLoader(WinDLL).LoadLibrary("C:\\Henry\\PythonProjects\\CED\\CEDS64ML\\x86\\ceds64int.dll")
+
+print(type(ced_lib))
+# s = "Hello, World"
+# c_s = c_char_p(s)
+print(ced_lib)
+print(ced_lib.S64Open.argtypes)  # this function opens .smr(x) files
+ced_lib.S64Open.argtypes = [POINTER(c_char)]
+print(ced_lib.S64Open.argtypes)  # this function opens .smr(x) files
+file_name = b"A1_spa_000.smrx"
+print(c_char_p(file_name))
+result = ced_lib.S64Open(file_name)
+print(result)
