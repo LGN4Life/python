b719b77b772c5fb443a35615edc09b7e19f59b2d
hjalitto <45777577+hjalitto@users.noreply.github.com>
2020-01-25 17:46:58 -0800

    finally figures out how to access C:\Henry\PythonProjects\CED\CEDS64ML\x86\ceds64int.dll... I think

--- CED/createCEDWrapper.py
+++ CED/createCEDWrapper.py
@@ -4,5 +4,7 @@ from ctypes import *
 import os
 import sys
 import struct
-
 print(os.getcwd())
+# lib = WinDLL("C:\\Henry\\PythonProjects\\CED\\CEDS64ML\\x64\\ceds64int.dll")
+LibraryLoader(WinDLL).LoadLibrary("C:\\Henry\\PythonProjects\\CED\\CEDS64ML\\x86\\ceds64int.dll")
+# lib = WinDLL("C:\\CEDMATLAB\\CEDS64ML\\x64\\ceds64int.dll")
