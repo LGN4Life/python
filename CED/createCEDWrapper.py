489a3882f0b581161bb993f4eb81ba527616a653
hjalitto <45777577+hjalitto@users.noreply.github.com>
2020-01-26 17:58:45 -0800

    making process. Can load file and extract channel titles!

--- CED/createCEDWrapper.py
+++ CED/createCEDWrapper.py
@@ -1,4 +1,3 @@
-print("hello world")
 
 from ctypes import *
 import os
@@ -14,8 +13,29 @@ print(type(ced_lib))
 print(ced_lib)
 print(ced_lib.S64Open.argtypes)  # this function opens .smr(x) files
 ced_lib.S64Open.argtypes = [POINTER(c_char)]
+ced_lib.S64Close.argtypes = [c_int]
 print(ced_lib.S64Open.argtypes)  # this function opens .smr(x) files
-file_name = b"A1_spa_000.smrx"
+file_name = b"rub_190719_con_023.smr"
 print(c_char_p(file_name))
-result = ced_lib.S64Open(file_name)
-print(result)
+fhand = ced_lib.S64Open(file_name)
+ced_lib.S64MaxChans.argtypes = [c_int]
+new_title = b"place holder"
+
+print('process start')
+ced_lib.S64SetChanTitle.argtypes = [c_int, c_int,POINTER(c_char)]
+chan_num = 25
+chan_buffer = ced_lib.S64GetChanTitle(fhand, chan_num, new_title, -1)
+arr = bytes(chan_buffer)
+print('arr  = ' + str(arr))
+print('type arr is ' + (str(type(arr))))
+print('type new_title is ' + (str(type(new_title))))
+get_title_result = ced_lib.S64GetChanTitle(fhand, chan_num, arr, chan_buffer)
+print('process finished')
+print("result of SetChan title  = " + str(arr))
+print("get_title_result = " + str(get_title_result))
+
+current_title = arr.decode("utf-8")
+print('new string  = ' + current_title)
+result = ced_lib.S64Close(fhand)
+
+
