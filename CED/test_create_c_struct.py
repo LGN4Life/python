abb086cb01a7e38764a81689d6a8527bc6e6c8ef
hjalitto <45777577+hjalitto@users.noreply.github.com>
2020-02-20 12:33:24 -0800

    can read in text markers

---
+++ CED/test_create_c_struct.py
@@ -0,0 +1,6 @@
+import ced_class
+
+
+a = ced_class.S64Marker(10)
+print(a.m_Code1)
+print('explore class')
--- CED/test_load_text_marker.py
+++ CED/test_load_text_marker.py
@@ -6,6 +6,7 @@ import struct
 import sys
 from matplotlib import pyplot as plt
 import cedLib
+import ced_class
 from time import sleep
 file_name = b"rub_190719_con_024.smr"
 
@@ -21,4 +22,52 @@ ced = cedLib.open_library(lib_path)
 #open data file
 fhand = ced.S64Open(file_name)
 
-ced.S64ChanType(fhand, text_channel)
+channel_type = ced.S64ChanType(fhand, text_channel)
+print('channel type = ' + str(channel_type))
+
+row_count = c_int(0)
+col_count = c_int(0)
+r_pointer = POINTER(c_int)
+c_pointer = POINTER(c_int)
+text_count = ced.S64GetExtMarkInfo(fhand, text_channel, r_pointer(row_count), c_pointer(col_count))
+
+no_mask = 0
+max_tick = ced.S64ChanMaxTime(fhand, text_channel)
+start_tick = c_longlong(0)
+
+text_marker = ced_class.S64Marker()
+t_pointer = POINTER(ced_class.S64Marker)
+string_buffer = create_string_buffer(row_count.value)
+# s_pointer = POINTER(c_char)
+
+#input: [file handle, channel number, pointer to a S64Marker object, pointer to hold the returned string,
+    # start tick, end tick, mask handle]
+text_list = []
+time_list = []
+start_tick = start_tick.value
+loop_count = 0
+while start_tick < max_tick:
+    start_time = ced.S64TicksToSecs(fhand, start_tick)
+    print('start_time  = ' + str(start_time))
+
+    ced.S64Read1TextMark(fhand, text_channel, t_pointer(text_marker), string_buffer, start_tick, max_tick, no_mask)
+    text_list.append(string_buffer.value)
+    print('start_tick  = ' +  str(start_tick))
+    print(' current string was found at : ' + str(text_marker.m_time))
+    if text_marker.m_time < start_tick:
+        break
+    start_tick = text_marker.m_time+1
+    loop_count +=1
+    if loop_count > 2000:
+        break
+
+    print('current string = ' +str(string_buffer.value))
+
+    print('loop count = ' + str(loop_count))
+
+
+
+
+result = ced.S64Close(fhand)
+print('file closed  = ' + str(result))
+print('now explore the data')
